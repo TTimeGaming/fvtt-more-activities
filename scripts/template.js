7f4f51ade81071b9608fc88a1844c458c08b6719
TTimeGaming <ttimegaming95@gmail.com>
2025-08-08 19:07:32 +0100

    Created template activity for easier creation

---
+++ scripts/template.js
@@ -0,0 +1,83 @@
+export class TemplateData {
+    static async init() {
+    }
+
+    static applyListeners(message, html) {
+    }
+}
+
+export class TemplateActivityData extends dnd5e.dataModels.activity.BaseActivityData {
+    static defineSchema() {
+        const fields = foundry.data.fields;
+        const schema = super.defineSchema();
+
+        return schema;
+    }
+}
+
+export class TemplateActivitySheet extends dnd5e.applications.activity.ActivitySheet {
+    /** @inheritdoc */
+    static DEFAULT_OPTIONS = {
+        classes: [ `dnd5e2`, `sheet`, `activity-sheet`, `activity-TEMPLATE` ],
+    };
+
+    /** @inheritdoc */
+    static PARTS = {
+        ...super.PARTS,
+        effect: {
+            template: `modules/more-activities/templates/TEMPLATE-effect.hbs`,
+            templates: [
+                ...super.PARTS.effect.templates,
+            ],
+        }
+    };
+
+    /** @inheritdoc */
+    async _prepareEffectContext(context) {
+        context = await super._prepareEffectContext(context);
+        return context;
+    }
+
+    /** @inheritdoc */
+    _onRender(context, options) {
+    }
+}
+
+export class TemplateActivity extends dnd5e.documents.activity.ActivityMixin(TemplateActivityData) {
+    static LOCALIZATION_PREFIXES = [...super.LOCALIZATION_PREFIXES, `DND5E.TEMPLATE`];
+
+    static metadata = Object.freeze(
+        foundry.utils.mergeObject(super.metadata, {
+            type: `macro`,
+            img: `modules/more-activities/icons/TEMPLATE.svg`,
+            title: `DND5E.ACTIVITY.Type.TEMPLATE`,
+            hint: `DND5E.ACTIVITY.Hint.TEMPLATE`,
+            sheetClass: TemplateActivitySheet
+        }, { inplace: false })
+    );
+
+    static defineSchema() {
+        return TemplateActivityData.defineSchema();
+    }
+
+    /**
+     * Execute the macro activity
+     * @param {ActivityUseConfiguration} config - Configuration data for the activity usage.
+     * @param {ActivityDialogConfiguration} dialog - Configuration data for the activity dialog.
+     * @param {ActivityMessageConfiguration} message - Configuration data for the activity message.
+     * @returns {Promise<ActivityUsageResults|void>}
+     */
+    async use(config, dialog, message) {
+        const results = await super.use(config, dialog, message);
+        // PERFORM ACTION HERE
+        return results;
+    }
+
+    /**
+     * Get the actor that owns this activity's item.
+     * @type {Actor5e|null
+     */
+    get actor() {
+        return this.item?.actor || null;
+    }
+}
