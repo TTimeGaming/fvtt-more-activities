d7e105e59eeef53084afafcf8b403b791cc5f59d
TTimeGaming <ttimegaming95@gmail.com>
2025-08-12 11:32:09 +0100

    Bug fix for applying effects to NPCs

--- scripts/utils/effects.js
+++ scripts/utils/effects.js
@@ -2,25 +2,31 @@ export class EffectsData {
     static async apply(activity, actors) {
         if (activity?.appliedEffects?.length === 0) return;
 
-        for (const actor of actors) {
-            const item = activity?.item;
-            for (const effectId of activity.appliedEffects) {
-                const effect = item?.effects?.get(effectId);
-                if (!effect) {
-                    console.warn(`Effect ${effectId} not found on item ${item?.name}`);
-                    continue;
-                }
-                
-                try {
-                    const effectData = effect.toObject();
-                    effectData.origin = item?.uuid;
-                    await actor.createEmbeddedDocuments(`ActiveEffect`, [ effectData ]);
-                    ui.notifications.info(`Applied ${effect.name} to ${actor.name}`);
-                }
-                catch (error) {
-                    console.error(`Failed to apply effect ${effect.name} to ${actor.name}:`, error);
-                    ui.notifications.error(`Failed to apply effect ${effect.name} to ${actor.name}`);
-                }
+        const item = activity?.item;
+        for (const effectId of activity.appliedEffects) {
+            const effect = item?.effects?.get(effectId);
+            if (!effect) {
+                console.warn(`Effect ${effectId} not found on item ${item?.name}`);
+                continue;
+            }
+            
+            let effectData = undefined;
+            try {
+                effectData = {
+                    ...effect.toObject(),
+                    disabled: false,
+                    transfer: false,
+                    origin: effect.uuid,
+                };
+            }
+            catch (error) {
+                console.error(`Failed to identify effect ${effect.name}:`, error);
+                continue;
+            }
+
+            for (const actor of actors) {
+                await (actor.isToken ? game.actors.get(actor.parent.actorId) : actor).createEmbeddedDocuments(`ActiveEffect`, [ effectData ]);
+                ui.notifications.info(`Applied ${effect.name} to ${actor.name}`);
             }
         }
     }
